82c343696acadb9afac8cd6cd4f384a9170a13f1
Labhesh Shah <shahlabhesh@gmail.com>
2024-07-01 18:39:45 +0545

    for deploy

--- src/main/java/com/labhesh/Todos/Todos/app/authentication/AuthenticationController.java
+++ src/main/java/com/labhesh/Todos/Todos/app/authentication/AuthenticationController.java
@@ -1,11 +1,34 @@
 package com.labhesh.Todos.Todos.app.authentication;
 
+import com.labhesh.Todos.Todos.exception.InternalServerException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication", description = "Endpoints for user authentication")
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @Operation(summary = "Login user")
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody @Valid UserAuthenticateDto userAuthenticateDto) throws InternalServerException {
+        return authenticationService.loginUser(userAuthenticateDto);
+    }
+
+    @Operation(summary = "Register a new user")
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody @Valid UserAuthenticateDto userRegisterDto) throws InternalServerException {
+        return authenticationService.registerUser(userRegisterDto);
+    }
 }
